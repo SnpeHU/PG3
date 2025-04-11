25fcc675cb478ceeca50464cb7a9eec4a6ac9ee0
kouhoukei <48913150+YukariHu@users.noreply.github.com>
2025-04-11 15:57:01 +0900

    11

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,8 +1,30 @@
 #include <stdio.h>
-#include <iostream>
+
+int Money(int hours)
+{
+	if (hours <= 1)
+	{
+		return 100;
+	}
+	return Money(hours - 1) * 2 - 50 + Money(hours - 1);
+
+}
+
+void Compare(int hours)
+{
+	for (int i = 1; i < hours; i++)
+	{
+		printf("時間: %d\n", i);
+		printf("一般的: %d\n", i * 1072);
+		printf("再帰的: %d\n", Money(i));
+
+	}
+}
+
 int main()
 {
-    printf("好き\n");
-    return 0;
-
+	int hours;
+	Compare(9);
+	
+	return 0;
 }
