d62ccb84b0d116359e802f352e19e2369be4ed9a
kouhoukei <48913150+YukariHu@users.noreply.github.com>
2025-04-11 15:37:27 +0900

    11

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,8 +1,25 @@
 #include <stdio.h>
-#include <iostream>
+
+template<typename T1,typename T2>
+T1 Min(T1 a, T2 b)
+{
+	return static_cast<T1>(a < b) ? a : b;
+}
+
+template<>
+char Min<char, char>(char a, char b)
+{
+	printf("数字以外は代入できません");
+	return 0;
+}
+
 int main()
 {
-    printf("好き\n");
+	printf("%d\n", Min(1, 2));
+	printf("%f\n", Min(5.33f, 2.5f));
+	printf("%lf\n", Min(5.4534, 8.432));
+	Min('a', 'b');
+
     return 0;
 
 }
