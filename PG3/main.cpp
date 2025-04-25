b413d428479f7e9340d5ce8e936c2ad51fec22ed
kouhoukei <48913150+YukariHu@users.noreply.github.com>
2025-04-25 14:50:57 +0900

    敵のフェーズ更新

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,43 +1,62 @@
-#include <stdio.h>
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <windows.h>  
+#include <functional>  
 
-/// <summary>
-/// 再帰的の場合、給料を計算する
-/// </summary>
-/// <param name="hours">働き時間</param>
-/// <returns>時給</returns>
-int Money(int hours)
+class Enemy  
+{  
+public:  
+	Enemy() = default;  
+	~Enemy() = default;  
+
+void Update()
 {
-	if (hours <= 1)
+	(this->*phaseTable[phase])();
+	phase++; // フェーズを進める
+	if (phase >= phaseCount) // フェーズが終わったら
 	{
-		return 100;
-	}
-	return Money(hours - 1) * 2 -50;
-
-}
-
-/// 一般的と再帰的の給料の比較
-void Compare(int hours)
-{
-	for (int i = 1; i < hours; i++)
-	{
-		printf("時間: %d\n", i);
-		printf("一般的: %d\n", i * 1072);
-
-		// 再帰的な計算
-		int money = 0;
-		for (int j = 1; j <= i; j++)
-		{
-			money += Money(j);
-		}
-		printf("再帰的: %d\n", money);
-
+		phase = 0; // フェーズをリセット
 	}
 }
 
-int main()
-{
-	int hours;
-	Compare(10);
-	
-	return 0;
+//接近フェーズ
+	void ApproachPhase()
+	{
+		printf("接近!\n");
+	}
+//射撃フェーズ
+	void ShootPhase()  
+	{  
+		printf("射撃!\n");  
+	}  
+
+//撤退フェーズ
+	void RetreatPhase()
+	{
+		printf("撤退!\n");
+	}
+
+private:
+	int phase = 0; // 現在のフェーズインデックス
+	static const int phaseCount; // フェーズの数
+	static void (Enemy::* phaseTable[])();  
+};  
+
+void (Enemy::* Enemy::phaseTable[])() = {  
+&Enemy::ApproachPhase,
+&Enemy::ShootPhase,
+&Enemy::RetreatPhase
+};  
+
+const int Enemy::phaseCount = sizeof(Enemy::phaseTable) / sizeof(Enemy::phaseTable[0]); // フェーズの数を計算
+
+int main()  
+{  
+	Enemy enemy;
+	while (true)
+	{
+		enemy.Update(); // 敵の状態を更新
+		Sleep(2000); // 2秒待機
+	}
+return 0;  
 }
