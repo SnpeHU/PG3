c5c56c5584b548d35fcc2f6f71111bee5b9f20fd
kouhoukei <48913150+YukariHu@users.noreply.github.com>
2025-05-09 14:26:41 +0900

    11

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -3,38 +3,8 @@
 #include <windows.h>  
 #include <functional>  
 
-template<typename T1, typename T2 = T1>
-class Compare {
-public:
-	Compare(T1 a, T2 b) : a(a), b(b) {};
-
-	auto Min()
-	{
-		return a < b ? a : b;
-	}
-private:
-	T1 a;
-	T2 b;
-};
-
 int main()  
 {  
-	Compare<int> c(10, 20);
-	Compare<float> c2(10.5f, 20.5f);
-	Compare<double> c3(6.5, 8.5);
-	Compare<int, float> c4(10, 20.5f);
-	Compare<int, double> c5(25, 20.5);
-	Compare<float, double> c6(10.5f, 20.0);
-
-
-
-
-	printf("小さいの数字は %d\n", c.Min());
-	printf("小さいの数字は %f\n", c2.Min());
-	printf("小さいの数字は %lf\n", c3.Min());
-	printf("小さいの数字は %f\n", c4.Min());
-	printf("小さいの数字は %lf\n", c5.Min());
-	printf("小さいの数字は %f\n", c6.Min());
-
+	
 return 0;  
 }
